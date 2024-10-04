959739e6d62eaaca2b608369defe8b0b787adbcf
9kf <kf.9francisco@gmail.com>
2024-10-04 22:36:38 +0800

    added netlify config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { createRoutesFromFolders } from "@remix-run/v1-route-convention";
+import { netlifyPlugin } from "@netlify/remix-adapter/plugin";
 import path from "node:path";
 
 export default defineConfig({
@@ -22,6 +23,7 @@ export default defineConfig({
         });
       },
     }),
+    netlifyPlugin(),
     tsconfigPaths(),
   ],
   resolve: {
