840d5978d65623514a0c445a28e9cfe16044955e
9kf <kf.9francisco@gmail.com>
2024-11-17 14:22:36 +0800

    maintenance page

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,26 +1,8 @@
 /* eslint-disable no-case-declarations */
 /* eslint-disable import/no-unresolved */
 import type { MetaFunction } from "@remix-run/node";
-import { json, useLoaderData, useLocation } from "@remix-run/react";
-import { useEffect, useMemo, useRef, useState } from "react";
 
-import { getHeroSectionImages } from "~/.server";
-import { getAsset } from "~/.server/api/assets";
-import { getBanner } from "~/.server/api/banner";
-import { getFAQs } from "~/.server/api/faqs";
-import { getReviews } from "~/.server/api/reviews";
-import { getTours } from "~/.server/api/tours";
-import { NavBar, TNavBarItems, WhatsAppFloatingButton } from "~/components";
-import { useScrollOffset } from "~/hooks/useScrollOffset";
-import {
-  AboutUs,
-  Expectations,
-  FAQ,
-  FeaturedServices,
-  Footer,
-  HeroSection,
-  Reviews,
-} from "~/layouts";
+import { Footer } from "~/layouts";
 
 export const meta: MetaFunction = () => {
   return [
@@ -29,176 +11,19 @@ export const meta: MetaFunction = () => {
   ];
 };
 
-export const loader = async () => {
-  const [heroImages, tours, reviews, faqs, videoHeadline, banner] =
-    await Promise.all([
-      getHeroSectionImages(),
-      getTours(),
-      getReviews(),
-      getFAQs(),
-      getAsset(5),
-      getBanner(),
-    ]);
-
-  return json({
-    heroImages: heroImages?.data || [],
-    tours: tours?.data || [],
-    reviews: reviews?.data || [],
-    faqs: faqs?.data || [],
-    videoHeadline: videoHeadline,
-    banner,
-  });
-};
-
 export default function Index() {
-  const { heroImages, tours, reviews, faqs, videoHeadline, banner } =
-    useLoaderData<typeof loader>();
-
-  const [activeLink, setActiveLink] = useState<TNavBarItems>("home");
-  const { state } = useLocation();
-
-  const scrollOffset = useScrollOffset();
-
-  const heroSectionRef = useRef<HTMLDivElement | null>(null);
-  const servicesSectionRef = useRef<HTMLDivElement | null>(null);
-  const aboutUsSectionRef = useRef<HTMLDivElement | null>(null);
-  const faqSectionRef = useRef<HTMLDivElement | null>(null);
-
-  const heroDetails = useMemo(() => {
-    return {
-      title: "PH KAWASAN CANYONEERING",
-      subTitle:
-        "Experience great heights of excitement and thrilling adventure in Kawasan, Cebu.",
-      ctaText: "BOOK AN APPOINTMENT",
-      ctaHandler: () => handleNavClick("services"),
-      backgroundImages: heroImages.map(
-        (heroimage) => heroimage.attributes.image.data.attributes.url
-      ),
-    };
-  }, [heroImages]);
-
-  const handleNavClick = (section: TNavBarItems) => {
-    const yOffset = -100;
-    let scrollToY = 0;
-
-    switch (section) {
-      case "home":
-        if (!heroSectionRef.current) return;
-
-        scrollToY =
-          heroSectionRef.current?.getBoundingClientRect()?.top + window.scrollY;
-        break;
-      case "services":
-        if (!servicesSectionRef.current) return;
-
-        scrollToY =
-          servicesSectionRef.current?.getBoundingClientRect()?.top +
-          window.scrollY +
-          yOffset;
-        break;
-      case "about us":
-        if (!aboutUsSectionRef.current) return;
-
-        scrollToY =
-          aboutUsSectionRef.current?.getBoundingClientRect()?.top +
-          window.scrollY +
-          yOffset;
-        break;
-      case "faq":
-        if (!faqSectionRef.current) return;
-
-        scrollToY =
-          faqSectionRef.current?.getBoundingClientRect()?.top +
-          window.scrollY +
-          yOffset;
-        break;
-    }
-
-    window.scrollTo({ top: scrollToY, behavior: "smooth" });
-  };
-
-  const handleOnScrolledCallback = (scrollOffset: number) => {
-    if (
-      !!servicesSectionRef.current &&
-      scrollOffset <
-        servicesSectionRef.current?.getBoundingClientRect()?.top +
-          window.screenY
-    ) {
-      setActiveLink("home");
-      return;
-    }
-
-    if (
-      !!heroSectionRef.current &&
-      scrollOffset >
-        heroSectionRef.current?.getBoundingClientRect()?.top + window.screenY &&
-      !!aboutUsSectionRef.current &&
-      scrollOffset <
-        aboutUsSectionRef.current?.getBoundingClientRect()?.top + window.screenY
-    ) {
-      setActiveLink("services");
-      return;
-    }
-
-    if (
-      !!servicesSectionRef.current &&
-      scrollOffset >
-        servicesSectionRef.current?.getBoundingClientRect()?.top +
-          window.screenY &&
-      !!faqSectionRef.current &&
-      scrollOffset <
-        faqSectionRef.current?.getBoundingClientRect()?.top +
-          window.screenY +
-          2500
-    ) {
-      setActiveLink("about us");
-      return;
-    }
-
-    if (
-      !!aboutUsSectionRef.current &&
-      scrollOffset >
-        aboutUsSectionRef.current?.getBoundingClientRect()?.top + window.screenY
-    ) {
-      setActiveLink("faq");
-      return;
-    }
-  };
-
-  useEffect(() => {
-    handleOnScrolledCallback(scrollOffset);
-  }, [scrollOffset]);
-
-  useEffect(() => {
-    if (!state?.section) return;
-
-    handleNavClick(state?.section);
-  }, [state]);
-
   return (
-    <div className="flex flex-col h-screen w-screen">
-      <NavBar
-        tours={tours}
-        activeLink={activeLink}
-        banner={banner}
-        onItemClick={handleNavClick}
-      />
-      <HeroSection
-        heroDetails={heroDetails}
-        videoHeadline={videoHeadline}
-        ref={heroSectionRef}
-      />
-      <FeaturedServices ref={servicesSectionRef} products={tours} />
-      <Expectations tours={tours.length > 0 ? tours.slice(0, 2) : []} />
-      <AboutUs ref={aboutUsSectionRef} />
-      <Reviews reviews={reviews} />
-      <FAQ ref={faqSectionRef} questions={faqs} />
-      <Footer
-        tours={tours}
-        onAboutUsClicked={() => handleNavClick("about us")}
-        onFAQClicked={() => handleNavClick("faq")}
-      />
-      <WhatsAppFloatingButton />
+    <div className="flex flex-col w-full h-full">
+      {/* <NavBar tours={[]} activeLink={"home"} elevateBackground={true} /> */}
+      <div className="flex flex-col h-auto px-8 md:px-12 lg:px-48 py-40 md:pt-56 items-center justify-center gap-2">
+        <h1 className="font-rushink text-center text-3xl md:text-5xl lg:text-7xl text-primary mb-10">
+          The site is under maintenance.
+        </h1>
+        <p className="text-center">
+          Please reach out to +6391772221704 for any inquiries.
+        </p>
+      </div>
+      <Footer tours={[]} />
     </div>
   );
 }
